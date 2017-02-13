8081daf8d924c31bc56423d5356b129fc76243b3
austenc <austen.cameron@gmail.com>
2017-02-13 09:47:02 -0700

    initial commit

---
+++ resources/assets/js/components/Example.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Example Component</div>
+
+                    <div class="panel-body">
+                        I'm an example component!
+                        <br>
+                        The version is {{ version }} -- this is from lumen
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                version: 0
+            }
+        },
+        created() {
+            axios.get('http://lumen-example.dev').then(function (response) {
+                this.version = response.data;
+            }.bind(this));
+        }
+    }
+</script>
